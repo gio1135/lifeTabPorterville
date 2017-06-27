dac18d5a4948378aa1c7d1bcd86b254c43a1a9f6
Giovanni Gallardo <giovannigallardo1135@gmail.com>
2017-06-26 22:58:43 -0700

    Fixed functionality

--- assets/js/init.js
+++ assets/js/init.js
@@ -30,3 +30,18 @@ $(document).ready(function() {
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
+
+// FAB
+jQuery(document).ready(function($){
+	$(window).scroll(function(){
+        if ($(this).scrollTop() < 200) {
+			$('.btn-floating') .fadeOut();
+        } else {
+			$('.btn-floating') .fadeIn();
+        }
+    });
+	$('.btn-floating').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 'fast');
+		return false;
+		});
+});
